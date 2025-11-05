81c30d0805d45895757e89b6cd25dc6a5223c7fb
Mizumo-prjkt <bluepearl394@gmail.com>
2025-11-05 04:09:28 +0000

    Add initial HTML login page, CSS styles, and update server configuration to serve static assets

--- qrnode-ui/server.js
+++ qrnode-ui/server.js
@@ -6,6 +6,23 @@ const port = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, 'assets'))); // Serve assets folder
+
+// Serve node_modules
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+
+// Serve the html files
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+// app.get('/login', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'login.html'));
+// });
+
+
+
+
 
 // A simple route for testing
 app.get('/api/hello', (req, res) => {
